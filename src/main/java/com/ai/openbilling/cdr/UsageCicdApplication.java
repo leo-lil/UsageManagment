3435cd05727c5067a67d4f957834d0a3f0a43fee
staunchcao <caohm5@asiainfo-int.com>
2021-11-20 14:00:44 +0800

    首次提交

---
+++ src/main/java/com/ai/openbilling/cdr/UsageCicdApplication.java
@@ -0,0 +1,13 @@
+package com.ai.openbilling.cdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsageCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsageCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/openbilling/cdr/UsageCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.openbilling.cdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
