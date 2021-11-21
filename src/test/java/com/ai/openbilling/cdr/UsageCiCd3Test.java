3db282b268075e2eb07001d2e7da1d47ca4c289d
staunchcao <caohm5@asiainfo-int.com>
2021-11-21 19:20:25 +0800

    junit版本

--- src/test/java/com/ai/openbilling/cdr/UsageCiCd3Test.java
+++ src/test/java/com/ai/openbilling/cdr/UsageCiCd3Test.java
@@ -1,23 +1,32 @@
 package com.ai.openbilling.cdr;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.Test;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.ai.openbilling.cdr.controller.UsageManagmentController;
-
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = UsageCicdApplication.class)
-public class UsageCiCdTest extends AbstractTestNGSpringContextTests {
+public class UsageCiCd3Test  {
 
 	@Autowired
 	private  UsageManagmentController UsageManagmentController;
-	
 	@Test
     public void testUsageQry(){
 		String fields="";
 		Integer offset=1;
 		Integer limit=10;
+		
 		System.out.println("testUsageQry");
+    }
+	
+	@Test
+    public void testUsageQry2(){
+		String fields="";
+		Integer offset=1;
+		Integer limit=10;
+		System.out.println("testUsageQry2");
     }
 }
